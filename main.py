5b1996706e99171ab222deb4a7abcfb9f14fe699
Onno1234 <hoeksmaonno@gmail.com>
2025-09-07 17:41:21 +0200

    word_counter

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("greetings boots")
+def main():
+    text = get_book_text('/home/onno/workspace/github.com/onno1234/bookbot/bookbot/books/frankenstein.txt')
+    num_words = count_words(text)
+    print(f'{num_words} words found in the document')
+
+def get_book_text(filepath):
+    with open(filepath) as file:
+        file_contents = file.read()
+    return file_contents
+
+def count_words(text):
+    counter = 0
+    text_list = text.split()
+    for word in text_list:
+        counter += 1
+    return counter
+
+main()
